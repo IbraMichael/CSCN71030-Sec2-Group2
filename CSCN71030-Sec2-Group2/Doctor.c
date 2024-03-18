c082d054da6f01aeb55928172f3854064164b7b6
Evalyne Juan <Evalynejuan44@gmail.com>
2024-03-17 23:32:19 -0400

    Doctor Commit # 3

---
+++ CSCN71030-Sec2-Group2/Doctor.c
@@ -0,0 +1,17 @@
+// CSCN71030-Sec2-24W
+// Group 2
+// Implementation of a hospital management software.
+// Conpiling Doctor ()
+// Evalyne Juan
+
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+struct Doctor {
+	char Name[15];
+	int age;
+	char gender;
+	int ID;
+
+};
