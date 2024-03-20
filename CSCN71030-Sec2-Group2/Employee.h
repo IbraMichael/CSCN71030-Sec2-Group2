963c2fd105c69350a6fe96efaf24ef2b9007cf13
OWEN_DAVIS <odavis2807@conestogac.on.ca>
2024-03-20 17:52:07 -0400

    Employee struct

--- CSCN71030-Sec2-Group2/Employee.h
+++ CSCN71030-Sec2-Group2/Employee.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Doctor.h"
+
+typedef enum role {
+	DOCTOR, NURSE, ADMIN
+}ROLE;
+
+typedef union data {
+	Doctor d;
+	Nurse n;
+}DATA;
+
+typedef struct employee {
+	ROLE role;
+	DATA data;
+	int id;
+};
