22e30c6a88225604c389511dd56f6a575902cd53
Evalyne Juan <Evalynejuan44@gmail.com>
2024-03-22 22:52:43 -0400

    commit #11

--- CSCN71030-Sec2-Group2/Doctor.h
+++ CSCN71030-Sec2-Group2/Doctor.h
@@ -5,10 +5,17 @@
 #define MAX_HISTORY 200
 
 // Define a struct for a doctor
+struct Doctor {
+	char name;
+	int id;
+	char email[50];
+
+
+
+}; Doctor;
 
 struct Patients {
 	char Name[40];
-	int age;
 	char address[300];
 	int id;
 
@@ -30,6 +37,7 @@ struct Patient patients[MAX_PATIENTS];
 struct History history[MAX_PATIENTS];
 
 // Functions
+void CreateDoctor(int id, char name, char email[50]) {}
 void addPatient(int id, char name[], char address[], int age) {}
 void addPatientHistory(int id, char symptom[200], int numSymptoms, char diagnosis[200], int prescription[200], int numPrescriptions){}
 void displayPatient(int id){}
