278710b784f280392f73aa950e52c46685442aae
Evalyne Juan <Evalynejuan44@gmail.com>
2024-03-22 22:39:09 -0400

    commit #10

--- CSCN71030-Sec2-Group2/Doctor.c
+++ CSCN71030-Sec2-Group2/Doctor.c
@@ -7,3 +7,16 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <string.h>
+
+void addPatient(int id, char name[], char address[], int age) {
+	return 0;
+}
+void addPatientHistory(int id, char symptom[200], int numSymptoms, char diagnosis[200], int prescription[200], int numPrescriptions) {
+	return 0;
+}
+void displayPatient(int id) {
+	return 0;
+}
+void displayPatientHistory(int id) {
+	return 0;
+}
--- CSCN71030-Sec2-Group2/Doctor.h
+++ CSCN71030-Sec2-Group2/Doctor.h
@@ -1,17 +1,38 @@
 #pragma once
 #include"Doctor.c"
 
+#define MAX_PATIENTS 500
+#define MAX_HISTORY 200
+
 // Define a struct for a doctor
 
-typedef struct {
-	char Name[50];
+struct Patients {
+	char Name[40];
 	int age;
-	char gender;
-	int diagnosis[100];
+	char address[300];
+	int id;
 
-} Doctor;
+};
 
-// Function prototypes
-void addDoctor(Doctor *doctors, int* numDoctors);
-void displayDoctors(Doctor* doctors, int numDoctors);
+// Structure to represent patient history
+struct History {
+	int patientID;
+	char symptoms[MAX_HISTORY][200];
+	int numSynptoms;
+	char diagnosis[MAX_HISTORY][200];
+	int numDiagnosis;
+	char prescriptions[MAX_HISTORY][200];
+	int numPrescriptions;
+};
+
+// Arrays to store patients and their histories
+struct Patient patients[MAX_PATIENTS];
+struct History history[MAX_PATIENTS];
+
+// Functions
+void addPatient(int id, char name[], char address[], int age) {}
+void addPatientHistory(int id, char symptom[200], int numSymptoms, char diagnosis[200], int prescription[200], int numPrescriptions){}
+void displayPatient(int id){}
+void displayPatientHistory(int id){}
+
 
